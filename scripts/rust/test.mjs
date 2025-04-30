7a7efd8e69f7c28c26b7e5d130d98f159baa2b3d
Jon C <me@jonc.dev>
2025-04-30 22:45:34 +0200

    CI: Add automated publish step (#91)
    * CI: Add automated publish step
    
    #### Problem
    
    We need to publish new ATA crates, but there's no publishing workflow in
    this repo.
    
    #### Summary of changes
    
    Copying the token repo for the most part:
    
    * add a publish-rust workflow
    * remove test-sbf script, update test.mjs to work with program-test
    * remove test-sbf feature from ATA program crate
    * add a publish script
    
    * Restore program builds to make things work

--- scripts/rust/test.mjs
+++ scripts/rust/test.mjs
@@ -1,11 +1,8 @@
 #!/usr/bin/env zx
 import 'zx/globals';
-import {
-  cliArguments,
-  workingDirectory,
-} from '../utils.mjs';
+import { cliArguments, workingDirectory } from '../utils.mjs';
 
 const [folder, ...args] = cliArguments();
+const sbfOutDir = path.join(workingDirectory, 'target', 'deploy');
 const manifestPath = path.join(workingDirectory, folder, 'Cargo.toml');
-
-await $`RUST_LOG=error cargo test --manifest-path ${manifestPath} ${args}`;
+await $`RUST_LOG=error SBF_OUT_DIR=${sbfOutDir} cargo test --manifest-path ${manifestPath} ${args}`;

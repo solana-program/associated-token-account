8ae867f2df86cc71b8b5c7365d407efc16aff785
rustopian <96253492+rustopian@users.noreply.github.com>
2025-07-10 22:54:36 +0100

    gt, not eq

--- p-ata/src/processor.rs
+++ p-ata/src/processor.rs
@@ -136,7 +136,7 @@ fn parse_ata_accounts(accounts: &[AccountInfo]) -> Result<AtaAccounts, ProgramEr
     // getting rent_info requires looking at len, so we might as well get a good
     // error for accounts < 6
     let rent_info = match accounts.len() {
-        7 => Some(unsafe { accounts.get_unchecked(6) }),
+        len if len >= 7 => Some(unsafe { accounts.get_unchecked(6) }),
         6 => None,
         _ => return Err(ProgramError::NotEnoughAccountKeys),
     };

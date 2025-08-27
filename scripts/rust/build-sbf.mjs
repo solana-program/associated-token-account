bcf0fedbdd26677af43058c00724307c22918c5e
rustopian <96253492+rustopian@users.noreply.github.com>
2025-08-27 17:32:18 +0100

    yank unnecessary condition

--- scripts/rust/build-sbf.mjs
+++ scripts/rust/build-sbf.mjs
@@ -7,5 +7,4 @@ import {
 
 const [folder, ...args] = cliArguments();
 const manifestPath = path.join(workingDirectory, folder, 'Cargo.toml');
-process.env.CARGO_TARGET_DIR ||= path.join(workingDirectory, 'target', 'sbf');
 await $`cargo-build-sbf --manifest-path ${manifestPath} ${args}`;

998ccf31a2a9a9cba4be4feef24070a306815ed4
Guillermo Bescos <g.bescos@yahoo.com>
2025-09-02 16:39:25 -0700

    use initialize4

--- program/src/processor.rs
+++ program/src/processor.rs
@@ -9,6 +9,7 @@ use {
     solana_program::{
         account_info::{next_account_info, AccountInfo},
         entrypoint::ProgramResult,
+        instruction::{AccountMeta, Instruction},
         msg,
         program::{invoke, invoke_signed},
         program_error::ProgramError,
@@ -22,10 +23,36 @@ use {
         instruction::AssociatedTokenAccountInstruction,
     },
     spl_token_2022_interface::{
+        check_spl_token_program_account,
         extension::{ExtensionType, StateWithExtensions},
         state::{Account, Mint},
     },
 };
+
+fn initialize_account4(
+    token_program_id: &Pubkey,
+    account_pubkey: &Pubkey,
+    mint_pubkey: &Pubkey,
+    owner_pubkey: &Pubkey,
+    close_authority_pubkey: &Pubkey,
+) -> Result<Instruction, ProgramError> {
+    check_spl_token_program_account(token_program_id)?;
+    let mut data = Vec::with_capacity(128);
+    data.push(25);
+    data.extend_from_slice(owner_pubkey.as_ref());
+    data.extend_from_slice(close_authority_pubkey.as_ref());
+
+    let accounts = vec![
+        AccountMeta::new(*account_pubkey, false),
+        AccountMeta::new_readonly(*mint_pubkey, false),
+    ];
+
+    Ok(Instruction {
+        program_id: *token_program_id,
+        accounts,
+        data,
+    })
+}
 
 /// Specify when to create the associated token account
 #[derive(PartialEq)]
@@ -147,12 +174,14 @@ fn process_create_associated_token_account(
             spl_token_program_info.clone(),
         ],
     )?;
+
     invoke(
-        &spl_token_2022_interface::instruction::initialize_account3(
+        &initialize_account4(
             spl_token_program_id,
             associated_token_account_info.key,
             spl_token_mint_info.key,
             wallet_account_info.key,
+            funder_info.key,
         )?,
         &[
             associated_token_account_info.clone(),

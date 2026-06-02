d02e4198c3e61fe8d60d56f4c94109a15ea33332
LStan <LStan@users.noreply.github.com>
2026-06-02 21:21:56 +0700

    remove thiserror

--- interface/src/error.rs
+++ interface/src/error.rs
@@ -1,15 +1,31 @@
 //! Error types
 
-use {num_derive::FromPrimitive, solana_program_error::ProgramError, thiserror::Error};
+use {
+    core::{error::Error, fmt},
+    num_derive::FromPrimitive,
+    solana_program_error::ProgramError,
+};
 
 /// Errors that may be returned by the program.
-#[derive(Clone, Debug, Eq, Error, FromPrimitive, PartialEq)]
+#[derive(Clone, Debug, Eq, FromPrimitive, PartialEq)]
 pub enum AssociatedTokenAccountError {
     // 0
     /// Associated token account owner does not match address derivation
-    #[error("Associated token account owner does not match address derivation")]
     InvalidOwner,
 }
+
+impl Error for AssociatedTokenAccountError {}
+
+impl fmt::Display for AssociatedTokenAccountError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            AssociatedTokenAccountError::InvalidOwner {} => {
+                f.write_str("Associated token account owner does not match address derivation")
+            }
+        }
+    }
+}
+
 impl From<AssociatedTokenAccountError> for ProgramError {
     fn from(e: AssociatedTokenAccountError) -> Self {
         ProgramError::Custom(e as u32)

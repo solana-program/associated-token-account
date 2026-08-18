26ad75a2c6e18756946118e4bef6184d711fb664
Loris Leiva <loris.leiva@gmail.com>
2026-08-19 00:27:56 +0100

    Use fully-qualified codama_macros::codama_program! path

--- pinocchio/interface/src/lib.rs
+++ pinocchio/interface/src/lib.rs
@@ -2,9 +2,6 @@
 
 #![no_std]
 
-#[cfg(feature = "codama")]
-use codama_macros::codama_program;
-
 pub mod error;
 pub mod instruction;
 pub mod pda;
@@ -12,4 +9,4 @@ pub mod pda;
 solana_address::declare_id!("ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL");
 
 #[cfg(feature = "codama")]
-codama_program!(name = "associatedTokenAccount");
+codama_macros::codama_program!(name = "associatedTokenAccount");
